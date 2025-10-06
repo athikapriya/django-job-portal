5dc9b88e9bb271e37dc769e0808b0d12d7b9f493
athikapriya <athikapriya1997@gmail.com>
2025-10-06 14:36:24 +0600

    Add logos list to home page view

--- base/views.py
+++ base/views.py
@@ -2,4 +2,14 @@ from django.shortcuts import render
 
 # Create your views here.
 def home_page(request):
-    return render(request, "base/index.html")
+    logos = [
+        'base/images/logo-visa.svg',
+        'base/images/logo-toyota.svg',
+        'base/images/logo-google.svg',
+        'base/images/logo-paypal.svg',
+        'base/images/logo-netflix.svg',
+        'base/images/logo-adidas.svg',
+        'base/images/logo-abbott.svg',
+        'base/images/logo-fedex.svg',
+    ]
+    return render(request, "base/index.html", {"logos" : logos})
