b4e915b5ba774db9b83a32094b6ff74674b30613
athikapriya <athikapriya1997@gmail.com>
2025-10-08 23:48:14 +0600

    register Job and Category to admin

--- jobs/admin.py
+++ jobs/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Job, Category
 
-# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+
+@admin.register(Job)
+class JObAdmin(admin.ModelAdmin):
+    list_display = ("title", "company", "category", "featured", "application_deadline")
+    list_filter = ("featured", 'category', 'job_type')
+    search_fields = ("title", "company")
