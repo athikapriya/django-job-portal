4bb8ff70572e4ec5355a7482c11a005b36c60f6a
athikapriya <athikapriya1997@gmail.com>
2025-10-08 23:50:06 +0600

    add urls for all_jobs

--- jobs/urls.py
+++ jobs/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("all-categories", views.jobs_by_category, name="jobs_by_category"), #for jobs by category
+    path('', views.all_jobs, name='all_jobs'),
 ]
