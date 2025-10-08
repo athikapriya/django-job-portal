122a6ab35d494801df8fd013cea5d956dc53327d
athikapriya <athikapriya1997@gmail.com>
2025-10-08 23:49:37 +0600

    add views for featured_jobs and all_jobs

--- jobs/views.py
+++ jobs/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from .models import Job, Category
 
 # Create your views here.
 def jobs_by_category(request):
     return render(request, "jobs/jobs_by_category.html")
+
+def featured_jobs(request):
+    featured_jobs = Job.objects.filter(featured=True).order_by("created_at")[:12]
+    return featured_jobs
+
+def all_jobs(request):
+    jobs = Job.objects.all().order_by("_created_at")
+    return render(request, "jobs/all_jobs.html", {"jobs" : jobs})
